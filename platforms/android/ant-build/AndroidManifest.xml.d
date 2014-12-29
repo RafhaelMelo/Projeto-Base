55f36e1d6318be7ae76ba2c15f7a8fe2d7d468cf
RafhaelMelo <rafhaelmelo@hotmail.com.br>
2014-12-29 13:00:30 -0200

    Platform

---
+++ platforms/android/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+C:\Users\Rafhael\Desktop\projeto\hello\platforms\android\ant-build\AndroidManifest.xml : \
+C:\Users\Rafhael\Desktop\projeto\hello\platforms\android\AndroidManifest.xml \
---
+++ platforms/android/ant-build/classes.dex.d
@@ -0,0 +1,11 @@
+C:\Users\Rafhael\Desktop\projeto\hello\platforms\android\ant-build\classes.dex : \
+C:\Users\Rafhael\Desktop\projeto\hello\platforms\android\ant-build\classes\com\example\hello\BuildConfig.class \
+C:\Users\Rafhael\Desktop\projeto\hello\platforms\android\ant-build\classes\com\example\hello\CordovaApp.class \
+C:\Users\Rafhael\Desktop\projeto\hello\platforms\android\ant-build\classes\com\example\hello\R$attr.class \
+C:\Users\Rafhael\Desktop\projeto\hello\platforms\android\ant-build\classes\com\example\hello\R$drawable.class \
+C:\Users\Rafhael\Desktop\projeto\hello\platforms\android\ant-build\classes\com\example\hello\R$string.class \
+C:\Users\Rafhael\Desktop\projeto\hello\platforms\android\ant-build\classes\com\example\hello\R$xml.class \
+C:\Users\Rafhael\Desktop\projeto\hello\platforms\android\ant-build\classes\com\example\hello\R.class \
+C:\Users\Rafhael\Desktop\projeto\hello\platforms\android\ant-build\classes\com\mundomed\www\CordovaApp.class \
+C:\Users\Rafhael\Desktop\projeto\hello\platforms\android\ant-build\classes\org\apache\cordova\splashscreen\SplashScreen.class \
+C:\Users\Rafhael\Desktop\projeto\hello\platforms\android\ant-build\dexedLibs\classes-18e632fb27dd112a4ba4f25173f8169c.jar \
